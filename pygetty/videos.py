9d0ece0e76f1ef6272c62a4cec39522402264361
Doug Cox <doug@lumen5.com>
2019-01-22 16:29:14 -0800

    Added pygetty.images and pygetty.videos.

---
+++ pygetty/videos.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import, unicode_literals
+
+from .assets import individual_asset, multiple_assets
+
+
+def video(
+    id,
+    detailed=False,
+    fields=set(),
+    query_params={},
+    auth_token_manager=None,
+    api_key=None,
+    client_secret=None,
+):
+    return individual_asset(
+        id,
+        'videos',
+        detailed=detailed,
+        fields=fields,
+        query_params=query_params,
+        auth_token_manager=auth_token_manager,
+        api_key=api_key,
+        client_secret=client_secret,
+    )
+
+
+def videos(
+    ids,
+    detailed=False,
+    fields=set(),
+    query_params={},
+    auth_token_manager=None,
+    api_key=None,
+    client_secret=None,
+):
+    return multiple_assets(
+        ids,
+        'videos',
+        detailed=detailed,
+        fields=fields,
+        query_params=query_params,
+        auth_token_manager=auth_token_manager,
+        api_key=api_key,
+        client_secret=client_secret,
+    )
